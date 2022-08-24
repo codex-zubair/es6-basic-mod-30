4961cdfcbd5f284bbfc488baa4fe86563d5e0394
Rocko <codingwithrock@gmail.com>
2022-08-24 19:28:02 +0600

    advance object delaration done

---
+++ js/adv-obj-dcl.js
@@ -0,0 +1,35 @@
+const object =  {
+    book3: " i'm book3  1st",
+    name3 : "book3 name",
+    lastName3: "my Last Name"
+};
+
+console.log(object.book3);
+
+
+
+// Advance object declare...
+console.log("i'm advance Second version of assign")
+const {book3,name3} = object;
+
+
+console.log(book3, name3);
+
+
+
+
+
+
+// advance object declare...
+const {book, name} =  {
+    book: " i'm book 2nd",
+    name : "book name 2nd",
+    lastName: "my Last Name"
+};
+
+console.log(book);
+console.log(name);
+
+
+
+
